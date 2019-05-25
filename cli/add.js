32680c699c2493a2e60c9a74a085bfb4c92ff982
Lars Thorup <lars@triggerz.com>
2019-05-25 10:43:45 +0200

    error handling

--- cli/add.js
+++ cli/add.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 const { Blob } = require('../lib/database/blob');
 const { Database } = require('../lib/database');
 const { Index } = require('../lib/index');
+const { LockDenied } = require('../lib/lockfile');
 const { Name } = require('../lib/name');
 const { Oid } = require('../lib/oid');
 const { Path } = require('../lib/path');
@@ -12,8 +13,6 @@ const { Workspace } = require('../lib/workspace');
 async function adding({ argv, cwd }) {
   const root = new Root(new Path(cwd()));
   const workspace = new Workspace({ path: root.path });
-  const database = new Database({ path: root.databasePath() });
-  const index = new Index({ path: root.indexPath() });
   const nameList = [];
   for (const arg of argv) {
     const path = Path.resolve(arg);
@@ -26,16 +25,27 @@ async function adding({ argv, cwd }) {
       await workspace.readingFileList({ path })
     );
   }
-  await index.updating(async () => {
-    for (const name of nameList) {
-      assert(name instanceof Name);
-      const { buffer: data, stat } = await workspace.readingFile({ name });
-      const blob = new Blob({ data });
-      await database.storing({ object: blob });
-      assert(blob.oid); // Note: created by database.storing()
-      index.add({ name, oid: blob.oid, stat });
+  try {
+    const database = new Database({ path: root.databasePath() });
+    const index = new Index({ path: root.indexPath() });
+    await index.updating(async () => {
+      for (const name of nameList) {
+        assert(name instanceof Name);
+        const { buffer: data, stat } = await workspace.readingFile({ name });
+        const blob = new Blob({ data });
+        await database.storing({ object: blob });
+        assert(blob.oid); // Note: created by database.storing()
+        index.add({ name, oid: blob.oid, stat });
+      }
+    });
+  } catch (error) {
+    if (error instanceof LockDenied) {
+      console.error(`fatal: ${error.message}`);
+      process.exit(1);
+    } else {
+      throw error;
     }
-  });
+  }
 }
 
 module.exports = {
