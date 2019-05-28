b4a21b218d3aa3a42ab688fd8f0adfa6d99b8896
Lars Thorup <lars@triggerz.com>
2019-05-28 16:28:41 +0200

    status: list untracked directory

--- cli/status.js
+++ cli/status.js
@@ -1,12 +1,69 @@
+const { Name } = require('../lib/name');
 const { Repository } = require('../lib/repository');
+
+async function isTrackable({ index, name, stat, workspace }) {
+  if (stat.isFile()) {
+    return !index.isTracked({ name });
+  }
+  if (!stat.isDirectory()) {
+    return false;
+  }
+  const { statByName } = await workspace.listingDirectory({
+    directoryName: name,
+  });
+  const fileNameList = Object.keys(statByName).filter(key =>
+    statByName[key].isFile()
+  );
+  const dirNameList = Object.keys(statByName).filter(key =>
+    statByName[key].isDirectory()
+  );
+  for (const nameString of [].concat(fileNameList, dirNameList)) {
+    const entryName = new Name(nameString);
+    const entryStat = statByName[nameString];
+    if (
+      await isTrackable({
+        name: entryName,
+        index,
+        stat: entryStat,
+        workspace,
+      })
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function scanning({ directoryName, index, workspace }) {
+  const untrackedNameList = [];
+  const { statByName } = await workspace.listingDirectory({ directoryName });
+  for (const nameString of Object.keys(statByName)) {
+    const name = new Name(nameString);
+    const stat = statByName[nameString];
+    if (index.isTracked({ name })) {
+      if (stat.isDirectory()) {
+        const { untrackedNameList: nameList } = await scanning({
+          directoryName: name,
+          index,
+          untrackedNameList,
+          workspace,
+        });
+        Array.prototype.push.apply(untrackedNameList, nameList);
+      }
+    } else if (await isTrackable({ index, name, stat, workspace })) {
+      const untrackedName = `${name.value}${stat.isDirectory() ? '/' : ''}`;
+      untrackedNameList.push(untrackedName);
+    }
+  }
+  return { untrackedNameList };
+}
 
 async function listingStatus({ console, cwd, exit }) {
   const { index, workspace } = Repository.at(cwd());
   await index.loading();
-  const nameList = await workspace.readingFileList();
-  const untrackedNameList = nameList.filter(name => !index.isTracked({ name }));
-  for (const name of untrackedNameList) {
-    console.log(`?? ${name.value}`);
+  const { untrackedNameList } = await scanning({ index, workspace });
+  for (const name of untrackedNameList.sort()) {
+    console.log(`?? ${name}`);
   }
   exit(0);
 }
--- cli/status.test.js
+++ cli/status.test.js
@@ -27,9 +27,36 @@ async function testing_status_untracked_when_not_in_index() {
   });
 }
 
+async function testing_status_untracked_directory_not_content() {
+  await NitTester.init(async nit => {
+    await nit.write('file.txt', '');
+    await nit.write('dir/another.txt', '');
+
+    await nit.cmd(['status']);
+
+    assert.equal(nit.stdout, ['?? dir/', '?? file.txt', ''].join('\n'));
+  });
+}
+
+async function testing_status_untracked_files_in_tracked_directory() {
+  await NitTester.init(async nit => {
+    await nit.write('a/b/inner.txt', '');
+    await nit.cmd(['add', '.']);
+    await nit.cmd(['commit'], { input: 'msg' });
+    await nit.write('a/outer.txt', '');
+    await nit.write('a/b/c/file.txt', '');
+
+    await nit.cmd(['status']);
+
+    assert.equal(nit.stdout, ['?? a/b/c/', '?? a/outer.txt', ''].join('\n'));
+  });
+}
+
 const testList = [
   testing_status_untracked_files_ordered,
   testing_status_untracked_when_not_in_index,
+  testing_status_untracked_directory_not_content,
+  testing_status_untracked_files_in_tracked_directory,
 ];
 
 async function testing() {
